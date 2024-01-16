9304e2bd23605a4fc4535e3961ec5881f51f8447
Gerd Marie Berger <gerdmarieberger@Gerds-MBP.uia.no>
2024-01-16 14:45:35 +0100

    la til en vits i app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+const log = console.log;
+
+log(chalk.magenta('Og så') + chalk.greenBright(' var det') + chalk.cyanBright(' svensken') 
++ chalk.blue(' som kom') + chalk.magenta(' to timer') + chalk.redBright(' for sent') 
++ chalk.green(' fordi') + chalk.blueBright(' rulletrappen hadde') 
++ chalk.yellowBright(' stoppet'));
+
+/* git remote add origin https://github.com/gmberger02/NodeTest.git
+git branch -M main
+git push -u origin main */
+
+
+
+
